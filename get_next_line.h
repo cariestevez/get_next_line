a0bce2c84c9a8bbd9b5ce24ac78bfbc3a4aaf0a1
cariestevez <116964367+cariestevez@users.noreply.github.com>
2023-02-01 21:14:33 +0100

    beginning

--- get_next_line.h
+++ get_next_line.h
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cestevez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/23 14:17:06 by cestevez          #+#    #+#             */
-/*   Updated: 2023/02/01 21:13:40 by cestevez         ###   ########.fr       */
+/*   Created: 2023/02/01 19:56:01 by cestevez          #+#    #+#             */
+/*   Updated: 2023/02/01 21:13:44 by cestevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#endif
 
-char	*get_next_line(int fd)
-{
-	void	*buff[BUFF_SIZE + 1];
-	char	new_line;
-	static char saved_line;
+#ifndef BUFF_SIZE 
+# define BUFF_SIZE  10
 
-	new_line = '\n';
-	*buff = read(fd, buff, BUFF_SIZE);
-	if (buff == 0);
-		return (0);
-	
-    return(buff)
-}
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
